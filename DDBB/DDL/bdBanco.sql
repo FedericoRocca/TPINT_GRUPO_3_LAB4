180f4ce9258907c5a2ac74598aec65b885585742
Andre <lpa24508@gmail.com>
2020-11-06 21:35:07 -0300

    BD carga de datos de Nationalities, Cities y Provinces

---
+++ DDBB/DDL/bdBanco.sql
@@ -0,0 +1,79 @@
+CREATE DATABASE bdBanco;
+
+USE bdBanco;
+
+CREATE TABLE Users
+(
+	 dni 		INT 		NOT NULL,
+     firstName 	VARCHAR(45) NOT NULL,
+     lastName 	VARCHAR(45) NOT NULL,
+     userName	VARCHAR(45) NOT NULL,
+     password	VARCHAR(45) NOT NULL,
+     cuil		VARCHAR(11) NOT NULL,
+     gender		VARCHAR(30) NOT NULL,
+     nationality VARCHAR(35) NOT NULL,
+     birthDate	DATETIME 	NOT NULL,
+     address 	VARCHAR(50) NOT NULL,
+     city		VARCHAR(45) 	NULL,
+     email		VARCHAR(50) NOT NULL,
+     status		BIT			NOT NULL,
+     
+	 PRIMARY KEY (dni)
+);
+
+CREATE TABLE Roles
+(
+	id		INT			AUTO_INCREMENT NOT NULL,
+	name 	VARCHAR(20)					NOT NULL, /* Admin ó Cliente */
+    status	BIT							NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Roles_x_Users
+(
+	dni		INT		NOT NULL,
+	roleId 	INT		NOT NULL, 
+    status	BIT		NOT NULL,
+    
+    PRIMARY KEY(dni, roleId),
+    FOREIGN KEY(dni) REFERENCES Users(dni),
+    FOREIGN KEY(roleId) REFERENCES Roles (id)
+);
+
+CREATE TABLE Phones
+(
+	numberPhone	INT 		NOT NULL,
+    description VARCHAR(20) NOT NULL, 	/* teléfono, celular, fax */
+    userDni		INT 		NOT NULL,
+    status		BIT			NOT NULL,
+     
+	PRIMARY KEY (numberPhone),
+    FOREIGN KEY(userDni) REFERENCES Users(dni)
+);
+
+CREATE TABLE Nationalities
+(
+	id			INT 	AUTO_INCREMENT	NOT NULL,
+    country 	VARCHAR(30) 			NOT NULL, 
+    gentilic	VARCHAR(40) 			NOT NULL,
+    iso			VARCHAR(3)				NULL,
+     
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Provinces
+(
+	id 			INT 	AUTO_INCREMENT 	NOT NULL,
+    description VARCHAR(50)				NOT NULL,
+    PRIMARY KEY (id)	
+);
+
+CREATE TABLE Cities
+(
+	id 			INT 	AUTO_INCREMENT 	NOT NULL,
+    description VARCHAR(45)				NOT NULL,
+    postalCode	VARCHAR(4) 				NOT	NULL,
+    provinceId	INT						NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (provinceId) REFERENCES Provinces (id)
+);
---
+++ DDBB/DML/recordsProvinces.sql
@@ -0,0 +1,29 @@
+USE bdBanco;
+
+-- 	Records of provincies --
+INSERT INTO Provinces (description) VALUES('Buenos Aires');
+INSERT INTO Provinces (description) VALUES('Capital Federal');
+INSERT INTO Provinces (description) VALUES('Catamarca');
+INSERT INTO Provinces (description) VALUES('Chaco');
+INSERT INTO Provinces (description) VALUES('Chubut');
+INSERT INTO Provinces (description) VALUES('Córdoba');
+INSERT INTO Provinces (description) VALUES('Corrientes');
+INSERT INTO Provinces (description) VALUES('Entre Ríos');
+INSERT INTO Provinces (description) VALUES('Formosa');
+INSERT INTO Provinces (description) VALUES('Jujuy');
+INSERT INTO Provinces (description) VALUES('La Pampa');
+INSERT INTO Provinces (description) VALUES('La Rioja');
+INSERT INTO Provinces (description) VALUES('Mendoza');
+INSERT INTO Provinces (description) VALUES('Misiones');
+INSERT INTO Provinces (description) VALUES('Neuquén');
+INSERT INTO Provinces (description) VALUES('Río Negro');
+INSERT INTO Provinces (description) VALUES('Salta');
+INSERT INTO Provinces (description) VALUES('San Juan');
+INSERT INTO Provinces (description) VALUES('San Luis');
+INSERT INTO Provinces (description) VALUES('Santa Cruz');
+INSERT INTO Provinces (description) VALUES('Santa Fé');
+INSERT INTO Provinces (description) VALUES('Santiago del Estero');
+INSERT INTO Provinces (description) VALUES('Tierra del Fuego');
+INSERT INTO Provinces (description) VALUES('Tucumán');
+
+SELECT * FROM Provinces
