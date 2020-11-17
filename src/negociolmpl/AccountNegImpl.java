00697c8149aabd4160316ff0dd1d04cb79d2dba6
abenitez573 <47705380+abenitez573@users.noreply.github.com>
2020-11-17 12:37:17 -0300

    Add files via upload

---
+++ src/negociolmpl/AccountNegImpl.java
@@ -0,0 +1,29 @@
+package negociolmpl;
+
+import java.util.ArrayList;
+
+import datos.IAccountDao;
+import datoslmpl.AccountDaoImpl;
+import dominio.Account;
+import negocio.IAccountNeg;
+
+public class AccountNegImpl implements IAccountNeg{
+
+	public IAccountDao accountDao = new AccountDaoImpl();
+	
+	public AccountNegImpl (IAccountDao accountDao) {
+		this.accountDao = accountDao;
+	}
+	
+	@Override
+	public ArrayList<Account> GetAllbyDni(String dni) {
+		return (ArrayList<Account>) accountDao.getAllbyDni(dni);
+		
+	}
+
+	@Override
+	public ArrayList<Account> GetAll() {
+		return (ArrayList<Account>) accountDao.getAll();
+	}
+
+}
