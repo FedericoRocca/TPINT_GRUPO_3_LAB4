7d8db7a0e2e5e389d5cab47b219f43dd6b263e9a
Federico 'Magus' Rocca <frocca17@gmail.com>
2020-11-20 16:35:14 -0300

    Corección de nacionalidad en usuarios administradores

--- DDBB/DML/DML.sql
+++ DDBB/DML/DML.sql
@@ -17,15 +17,15 @@ INSERT INTO roles (name, status) VALUES ("Cliente", 1);
 
 -- Administradores de prueba -- 
 INSERT INTO users (dni, firstName, lastName, userName, password, cuil, gender, nationality, birthDate, address, city, email, status) 
-	VALUES (99999999,"André","Villalta", "andre007", "007","23999999991","Masculino","Peruvian","1992-11-05", "calle 123", "Don Torcuato", "andre@gmail.com", 1 );
+	VALUES (99999999,"André","Villalta", "andre007", "007","23999999991","Masculino","1","1992-11-05", "calle 123", "Don Torcuato", "andre@gmail.com", 1 );
 INSERT INTO bdbanco.roles_x_users (`dni`,`roleId`,`status`) VALUES ('99999999',1,1);
 
 INSERT INTO users (dni, firstName, lastName, userName, password, cuil, gender, nationality, birthDate, address, city, email, status) 
-	VALUES ("99999988","Alan","Benitez", "alan008", "007","23999999881","Masculino","Argentina","1991-01-01", "calle 123", "Benavidez", "alan@gmail.com", 1 );
+	VALUES ("99999988","Alan","Benitez", "alan008", "007","23999999881","Masculino","2","1991-01-01", "calle 123", "Benavidez", "alan@gmail.com", 1 );
 INSERT INTO bdbanco.roles_x_users (`dni`,`roleId`,`status`) VALUES ('99999988',1,1);
 
 INSERT INTO users (dni, firstName, lastName, userName, password, cuil, gender, nationality, birthDate, address, city, email, status) 
-	VALUES ("36249161","Federico","Rocca", "usernameLoco", "Password","20362491615","Masculino","Argentina","1991-11-22", "Una calle random", "San Fernando", "frocca17@gmail.com", 1 );
+	VALUES ("36249161","Federico","Rocca", "usernameLoco", "Password","20362491615","3","Argentina","1991-11-22", "Una calle random", "San Fernando", "frocca17@gmail.com", 1 );
 INSERT INTO bdbanco.roles_x_users (`dni`,`roleId`,`status`) VALUES ('36249161',1,1);
 
 -- Teléfonos de prueba - Administradores --
